f99b210d53eff1580bf57c2fe1fd298539136176
Aerinas <49651840+Aerinas@users.noreply.github.com>
2024-07-03 22:21:50 +1000

    Create 453.最小操作次数使数组元素相同.py

---
+++ 数组/453.最小操作次数使数组元素相同.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def minMoves(self, nums):
+        sum = 0
+        minmum = min(nums)
+        for i in nums:
+            sum += i-minmum
+        return sum
+        
